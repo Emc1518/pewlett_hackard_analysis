2ef032783116337194c463e8338152afee8964d5
Emc1518 <81889167+Emc1518@users.noreply.github.com>
2021-06-02 00:32:53 -0400

    you know what it is

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,11 @@
+-- Retrieve the emp_no, first_name, and last_name columns from the Employees table.
+SELECT
+FROM 
+WHERE
+
+
+-- Retrieve the title, from_date, and to_date columns from the Titles table.
+-- Create a new table using the INTO clause.
+-- Join both tables on the primary key.
+-- Filter the data on the birth_date column to retrieve the employees who were born between 1952 and 1955. Then, order by the employee number.
+-- Export the Retirement Titles table from the previous step as retirement_titles.csv and save it to your Data folder in the Pewlett-Hackard-Analysis folder.
